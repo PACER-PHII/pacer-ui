34273c64aa6a952475c8b28d3cc3bd6b1e083d5f
Plamen Tassev <plamen@gatech.edu>
2024-05-04 15:23:05 -0400

    Render the version for pacer in the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {Router} from "@angular/router";
-
+import  packageInfo from 'package.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,6 +9,7 @@ import {Router} from "@angular/router";
 
 export class AppComponent {
   title = 'pacer-ui';
+  version = packageInfo.version;
   constructor(
     private router: Router,
   ){}
